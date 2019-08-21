f48064d5d2ce53b8f6557b916e6cec17e57388e8
Lucas Weyne <weynelucas@gmail.com>
2019-08-21 16:09:40 -0300

    Update endpoint

--- src/app/controllers/user-controller.js
+++ src/app/controllers/user-controller.js
@@ -7,6 +7,7 @@ class UserController {
       name: yup.string().required(),
       email: yup
         .string()
+        .email()
         .required()
         .test(
           'is-unique',
@@ -27,7 +28,7 @@ class UserController {
           password
             ? field.oneOf(
                 [yup.ref('password')],
-                "The two password fields didn't match."
+                "the two password fields didn't match."
               )
             : field
         ),
@@ -41,6 +42,57 @@ class UserController {
 
     const { id, email, name } = await User.create(req.body);
     return res.status(201).json({
+      id,
+      name,
+      email,
+    });
+  }
+
+  async update(req, res) {
+    const schema = yup.object().shape({
+      email: yup
+        .string()
+        .email()
+        .test(
+          'is-unique',
+          'a user is already registered with this e-mail address.',
+          async email => {
+            if (email !== req.user.email) {
+              const user = await User.findOne({ where: { email } });
+              return user === null;
+            }
+            return true;
+          }
+        ),
+      password: yup.string().min(6),
+      confirmPassword: yup
+        .string()
+        .when('password', (password, field) =>
+          password ? field.required() : field
+        ),
+      oldPassword: yup
+        .string()
+        .when('password', (password, field) =>
+          password ? field.required() : field.test()
+        )
+        .test(
+          'is-invalid',
+          'Your old password was entered incorrectly',
+          async value => {
+            req.user.checkPassword(value);
+          }
+        ),
+    });
+
+    try {
+      await schema.validate(req.body);
+    } catch (err) {
+      return res.status(400).json({ errors: err.errors });
+    }
+
+    const { id, name, email } = req.user.update(req.body);
+
+    return res.json({
       id,
       name,
       email,
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
+import authMiddleware from './app/middlewares/auth-middleware';
 import userController from './app/controllers/user-controller';
 import sessionController from './app/controllers/session-controller';
 
 const router = Router();
 
-router.post('/users', userController.store);
 router.post('/login', sessionController.store);
+router.post('/users', userController.store);
+router.put('/users', authMiddleware, userController.update);
 
 export default router;
