866c794d126ec54a87074706e0132d6021dd3cdb
Lucas Weyne <weynelucas@gmail.com>
2019-08-21 10:13:46 -0300

    Validate login

--- src/app/controllers/session-controller.js
+++ src/app/controllers/session-controller.js
@@ -1,9 +1,21 @@
+import * as yup from 'yup';
 import jwt from 'jsonwebtoken';
 import config from '../../config/auth';
 import User from '../models/user';
 
 class SessionControler {
   async store(req, res) {
+    const schema = yup.object().shape({
+      email: yup.string().required(),
+      password: yup.string().required(),
+    });
+
+    try {
+      await schema.validate(req.body);
+    } catch (err) {
+      return res.status(400).json({ errors: err.errors });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({
