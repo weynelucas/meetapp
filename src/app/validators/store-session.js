819592000bcc9987b73ef12f69922c4b6e3bf81e
Lucas Weyne <weynelucas@gmail.com>
2019-08-23 08:27:11 -0300

    trim sessionstore

--- src/app/validators/store-session.js
+++ src/app/validators/store-session.js
@@ -3,8 +3,14 @@ import * as yup from 'yup';
 export default class StoreSession {
   static getRules() {
     return yup.object().shape({
-      email: yup.string().required(),
-      password: yup.string().required(),
+      email: yup
+        .string()
+        .trim()
+        .required(),
+      password: yup
+        .string()
+        .trim()
+        .required(),
     });
   }
 }
