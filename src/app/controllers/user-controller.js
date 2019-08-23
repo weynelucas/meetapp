1849b045039ffb3bad37deda2c1cd19a68519c8f
Lucas Weyne <weynelucas@gmail.com>
2019-08-23 08:38:05 -0300

    Remove schema usercontroller

--- src/app/controllers/user-controller.js
+++ src/app/controllers/user-controller.js
@@ -1,9 +1,8 @@
-import * as yup from 'yup';
 import User from '../models/user';
 
 class UserController {
   async store(req, res) {
-    const { id, email, name } = await User.create(req.data);
+    const { id, email, name } = await User.create(req.body);
     return res.status(201).json({
       id,
       name,
@@ -12,57 +11,7 @@ class UserController {
   }
 
   async update(req, res) {
-    const schema = yup.object().shape({
-      name: yup.string(),
-      email: yup
-        .string()
-        .email()
-        .test(
-          'is-unique',
-          'a user is already registered with this e-mail address.',
-          async email => {
-            if (email && email !== req.user.email) {
-              const user = await User.findOne({ where: { email } });
-              return user === null;
-            }
-            return true;
-          }
-        ),
-      password: yup.string().min(6),
-      confirmPassword: yup
-        .string()
-        .when('password', (password, field) =>
-          password
-            ? field
-                .required()
-                .oneOf(
-                  [yup.ref('password')],
-                  "the two password fields didn't match."
-                )
-            : field
-        ),
-      oldPassword: yup
-        .string()
-        .when('password', (password, field) =>
-          password
-            ? field
-                .required()
-                .test(
-                  'is-invalid',
-                  'your old password was entered incorrectly',
-                  value => req.user.checkPassword(value)
-                )
-            : field
-        ),
-    });
-
-    try {
-      await schema.validate(req.body);
-    } catch (err) {
-      return res.status(400).json({ errors: err.errors[0] });
-    }
-
-    const { id, name, email } = await req.user.update(req.body);
+    const { id, name, email } = await req.user.update(req.data);
 
     return res.json({
       id,
