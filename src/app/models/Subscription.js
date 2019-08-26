52687c09e54bd3d861fbd0612e4177629a4889fd
Lucas Weyne <weynelucas@gmail.com>
2019-08-26 18:08:12 -0300

    Subscription model

---
+++ src/app/models/Subscription.js
@@ -0,0 +1,27 @@
+import { Model } from 'sequelize';
+
+export default class Subscription extends Model {
+  static init(sequelize) {
+    super.init(
+      {},
+      {
+        sequelize,
+        tableName: 'subscription',
+      }
+    );
+
+    return this;
+  }
+
+  static associate({ User, Meetup }) {
+    this.belongsTo(User, {
+      as: 'user',
+      foreignKey: { name: 'userId', field: 'user_id' },
+    });
+
+    this.belongsTo(Meetup, {
+      as: 'meetup',
+      foreignKey: { name: 'meetupId', field: 'meetup_id' },
+    });
+  }
+}
--- src/database/index.js
+++ src/database/index.js
@@ -3,8 +3,9 @@ import config from '../config/database';
 import User from '../app/models/User';
 import Meetup from '../app/models/Meetup';
 import File from '../app/models/File';
+import Subscription from '../app/models/Subscription';
 
-const models = [User, Meetup, File];
+const models = [User, Meetup, File, Subscription];
 
 class Database {
   constructor() {
