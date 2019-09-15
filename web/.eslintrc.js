2b87cb959e064de189b67914118da9b115097dc7
Lucas Weyne <weynelucas@gmail.com>
2019-09-15 13:12:21 -0300

    chore: add eslint plugins and rules

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,25 +1,34 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
-  extends: [
-    'airbnb',
-  ],
+  extends: ["airbnb", "prettier", "prettier/react"],
+  parser: "babel-eslint",
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module"
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ["react", "prettier", "react-hooks"],
   rules: {
-  },
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": [
+      "warn",
+      {
+        extensions: [".jsx", ".js"]
+      }
+    ],
+    "import/prefer-default-export": "off",
+    "no-param-reassign": "off",
+    "no-console": ["error", { allow: ["tron"] }],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  }
 };
