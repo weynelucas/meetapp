2994f958de091f201d461319a8dce034947daa2c
Lucas Weyne <weynelucas@gmail.com>
2019-09-15 14:24:18 -0300

    chore: config routes structure

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
@@ -15,9 +17,9 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" component={SignUp} />
 
-      <Route path="/dashboard" component={Dashborad} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/meetup" component={Meetup} />
+      <Route path="/dashboard" component={Dashborad} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
+      <Route path="/meetup" component={Meetup} isPrivate />
     </Switch>
   );
 }
