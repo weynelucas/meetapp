ae0ea7638a1198e09ac4a1f58f2e5d6c6ec96d55
Lucas Weyne <weynelucas@gmail.com>
2019-09-15 19:07:26 -0300

    feat: signup form

--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,7 +1,57 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Input, Form } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '../../assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Este campo é obrigatório.'),
+  email: Yup.string()
+    .trim()
+    .email('Insira um endereço de e-mail válido.')
+    .required('Este campo é obrigatório.'),
+  password: Yup.string()
+    .trim()
+    .min(6, 'A senha deve conter no mínimo 6 caracteres.')
+    .required('Este campo é obrigatório.'),
+  confirmPassword: Yup.string()
+    .required('Este campo é obrigatório.')
+    .when('password', (password, field) =>
+      password
+        ? field.oneOf(
+            [Yup.ref('password')],
+            'Os dois campos de senha não coincidem.',
+          )
+        : field,
+    ),
+});
 
 export default function SignUp() {
-  return <h1>SignUp</h1>;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="Meetapp" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Nome completo" />
+        <Input name="email" type="text" placeholder="Digite seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirme sua senha secreta"
+        />
+        <button type="submit">Entrar</button>
+      </Form>
+      <Link to="/">Já tenho login</Link>
+    </>
+  );
 }
