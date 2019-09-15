16dbf4881b4f1681caffb561811ca3d1f20b49ce
Lucas Weyne <weynelucas@gmail.com>
2019-09-15 14:55:36 -0300

    chore: add layouts

---
+++ web/src/pages/_layout/auth/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100%;
+  max-width: 315px;
+  margin: auto;
+`;
--- web/src/routes/Route.js
+++ web/src/routes/Route.js
@@ -1,8 +1,15 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import AuthLayout from '../pages/_layout/auth';
+import DefaulLayout from '../pages/_layout/default';
 
-export default function RouteWrapper({ component, isPrivate, ...rest }) {
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
   const signed = false;
 
   if (!signed && isPrivate) {
@@ -13,8 +20,18 @@ export default function RouteWrapper({ component, isPrivate, ...rest }) {
     return <Redirect to="/dashboard" />;
   }
 
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Route {...rest} component={component} />;
+  const Layout = isPrivate ? DefaulLayout : AuthLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouteWrapper.propTypes = {
