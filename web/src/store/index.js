1fb11c31b704d9ce461391e6c5dafe5adc19be3e
Lucas Weyne <weynelucas@gmail.com>
2019-09-15 21:16:36 -0300

    chore: move createStore logic

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,10 +1,11 @@
-import { createStore, compose, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
 import { persistStore } from 'redux-persist';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
+
+import createStore from './createStore';
 import persistReducers from './persistReducers';
 
 const middlewares = [];
@@ -18,15 +19,7 @@ const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 middlewares.push(sagaMiddleware);
 
-const composer =
-  process.env.NODE_ENV === 'development'
-    ? compose(
-        applyMiddleware(...middlewares),
-        console.tron.createEnhancer(),
-      )
-    : compose(applyMiddleware(...middlewares));
-
-const store = createStore(persistReducers(rootReducer), composer);
+const store = createStore(persistReducers(rootReducer), middlewares);
 const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
