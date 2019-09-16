59ef507e90e1cc2c9e2c0dc42f552879350725db
Lucas Weyne <weynelucas@gmail.com>
2019-09-15 22:45:53 -0300

    chore: dispatch signout action

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import logo from '../../assets/logo.svg';
 
 import { Container, Content, Profile } from './styles';
+import { signOut } from '../../store/modules/auth/actions';
 
 export default function Header() {
+  const dispatch = useDispatch();
+
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
   return (
     <Container>
       <Content>
@@ -16,10 +26,12 @@ export default function Header() {
         <nav>
           <Profile>
             <div>
-              <strong>Lucas Weyne</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
-            <button type="button">Sair</button>
+            <button type="button" onClick={handleSignOut}>
+              Sair
+            </button>
           </Profile>
         </nav>
       </Content>
