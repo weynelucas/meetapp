122f9c41819ea281b6750ac0765535f72fb49781
Lucas Weyne <weynelucas@gmail.com>
2019-09-20 07:12:04 -0300

    styles: meetup page structure

--- web/src/pages/Meetup/index.js
+++ web/src/pages/Meetup/index.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import { MdEdit, MdDeleteForever, MdEventNote, MdPlace } from 'react-icons/md';
 
-// import { Container } from './styles';
+import Button from '../../components/Button';
+import { Container, MeetupHeader, MeetupBody, MeetupFooter } from './styles';
 
 export default function Meetup() {
-  return <h1>Meetup</h1>;
+  return (
+    <Container>
+      <MeetupHeader>
+        Meetup de React Native
+        <div>
+          <Button theme="info">
+            <MdEdit size={20} />
+            Editar
+          </Button>
+
+          <Button>
+            <MdDeleteForever size={20} />
+            Cancelar
+          </Button>
+        </div>
+      </MeetupHeader>
+
+      <MeetupBody>
+        <img
+          src="http://localhost:3333/files/ba7fd1ff0311644705d6a2519b32f0ef.png"
+          alt="Meetup de React Native"
+        />
+        <p>
+          A maior conferência do ecossistema React da América Latina. Como nas
+          conferências dos Estados Unidos e Europa, reunimos desenvolvedores,
+          empresas e estudantes de tecnologia para compartilhar experiências no
+          desenvolvimento de aplicações reativas.,
+        </p>
+      </MeetupBody>
+
+      <MeetupFooter>
+        <span>
+          <MdEventNote color="rgba(255,255,255,0.6)" size={20} />
+          24 de Junho, às 20h
+        </span>
+        <span>
+          <MdPlace color="rgba(255,255,255,0.6)" size={20} />
+          Rua Guilherme Gembala, 260
+        </span>
+      </MeetupFooter>
+    </Container>
+  );
 }
---
+++ web/src/pages/Meetup/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const MeetupHeader = styled.h1``;
+
+export const MeetupBody = styled.div``;
+
+export const MeetupFooter = styled.footer``;
