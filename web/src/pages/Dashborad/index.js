b65dddc2b5af08966de717dba5432a4c49cf1b8e
Lucas Weyne <weynelucas@gmail.com>
2019-09-20 15:11:37 -0300

    chore: remove unused import

--- web/src/pages/Dashborad/index.js
+++ web/src/pages/Dashborad/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { MdKeyboardArrowRight, MdAddCircleOutline } from 'react-icons/md';
 
 import Button from '../../components/Button';
