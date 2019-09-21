5e228f6ee8bf587191a54f9b1417e0e71d8613ac
Lucas Weyne <weynelucas@gmail.com>
2019-09-21 14:43:57 -0300

    refactor: profile page use form component

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { MdAddCircleOutline } from 'react-icons/md';
 
+import { updateProfileRequest } from '../../store/modules/user/actions';
+
+import Form from '../../components/Form';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { ProfileForm } from './styles';
-import { updateProfileRequest } from '../../store/modules/user/actions';
+
+import { Container } from './styles';
 
 export default function Profile() {
   const dispatch = useDispatch();
@@ -18,32 +21,34 @@ export default function Profile() {
   }
 
   return (
-    <ProfileForm initialData={profile} onSubmit={handleSubmit}>
-      <Input name="name" type="text" placeholder="Nome completo" />
-      {errors.name && <span>{errors.name[0]}</span>}
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Nome completo" />
+        {errors.name && <span>{errors.name[0]}</span>}
 
-      <Input name="email" type="email" placeholder="E-mail" />
-      {errors.email && <span>{errors.email[0]}</span>}
+        <Input name="email" type="email" placeholder="E-mail" />
+        {errors.email && <span>{errors.email[0]}</span>}
 
-      <hr />
+        <hr />
 
-      <Input name="oldPassword" type="password" placeholder="Senha atual" />
-      {errors.oldPassword && <span>{errors.oldPassword[0]}</span>}
+        <Input name="oldPassword" type="password" placeholder="Senha atual" />
+        {errors.oldPassword && <span>{errors.oldPassword[0]}</span>}
 
-      <Input name="password" type="password" placeholder="Nova senha" />
-      {errors.password && <span>{errors.password[0]}</span>}
+        <Input name="password" type="password" placeholder="Nova senha" />
+        {errors.password && <span>{errors.password[0]}</span>}
 
-      <Input
-        name="confirmPassword"
-        type="password"
-        placeholder="Confirmação de senha"
-      />
-      {errors.confirmPassword && <span>{errors.confirmPassword[0]}</span>}
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmação de senha"
+        />
+        {errors.confirmPassword && <span>{errors.confirmPassword[0]}</span>}
 
-      <Button type="submit">
-        <MdAddCircleOutline size={20} color="#fff" />
-        Salvar perfil
-      </Button>
-    </ProfileForm>
+        <Button type="submit">
+          <MdAddCircleOutline size={20} color="#fff" />
+          Salvar perfil
+        </Button>
+      </Form>
+    </Container>
   );
 }
--- web/src/pages/Profile/styles.js
+++ web/src/pages/Profile/styles.js
@@ -1,23 +1,15 @@
 import styled from 'styled-components';
-import { Form } from '@rocketseat/unform';
 
-export const ProfileForm = styled(Form)`
-  display: flex;
-  flex-direction: column;
-
-  input {
-    & + input {
-      margin-top: 10px;
+export const Container = styled.div`
+  form {
+    hr {
+      border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+      margin: 30px 0 20px 0;
     }
-  }
 
-  hr {
-    border-bottom: 1px solid rgba(255, 255, 255, 0.1);
-    margin: 30px 0 20px 0;
-  }
-
-  button {
-    margin-top: 20px;
-    align-self: flex-end;
+    button {
+      margin-top: 20px;
+      align-self: flex-end;
+    }
   }
 `;
