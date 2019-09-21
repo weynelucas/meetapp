e4259109a00dae7dd1b4182bbe56d663d4c8d138
Lucas Weyne <weynelucas@gmail.com>
2019-09-21 15:10:12 -0300

    chore: dispatch create event

--- web/src/pages/Meetup/Create/index.js
+++ web/src/pages/Meetup/Create/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { addMeetupRequest } from '../../../store/modules/meetups/actions';
+
 import MeetupForm from '../Form';
 
 export default function CreateMeetup() {
+  const dispatch = useDispatch();
+
   function handleSubmit(data) {
-    console.tron.log(data);
+    dispatch(addMeetupRequest(data));
   }
 
   return <MeetupForm onSubmit={handleSubmit} />;
