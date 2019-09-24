ae1e03dfa910f2eabe1793598ff8950ed15e0c27
Lucas Weyne <weynelucas@gmail.com>
2019-09-24 19:48:00 -0300

    chore: root component

---
+++ app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StatusBar, View} from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <View />
+    </>
+  );
+}
