2430ebac1c2e9993c6fdd056f1537d9216aecec5
Lucas Weyne <weynelucas@gmail.com>
2019-09-25 16:32:13 -0300

    fix: persist gate import

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
