a8c6b8f6abf4f755121d762112d78b8ff711fa70
Lucas Weyne <weynelucas@gmail.com>
2019-09-26 20:48:10 -0300

    feat: header

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import Subscriptions from './pages/Subscriptions';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default isSignedIn =>
   createAppContainer(
@@ -15,30 +18,40 @@ export default isSignedIn =>
           SignIn,
           SignUp,
         }),
-        App: createBottomTabNavigator(
+        App: createStackNavigator(
           {
-            Dashboard,
-            Subscriptions,
-            Profile,
+            Container: createBottomTabNavigator(
+              {
+                Dashboard,
+                Subscriptions,
+                Profile,
+              },
+              {
+                tabBarOptions: {
+                  keyboardHidesTabBar: true,
+                  activeTintColor: '#FFF',
+                  inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+                  labelStyle: {
+                    fontSize: 12,
+                    marginTop: 5,
+                  },
+                  style: {
+                    backgroundColor: '#2B1A2F',
+                    paddingHorizontal: 50,
+                    paddingTop: 13,
+                    paddingBottom: 12,
+                    borderTopWidth: 0,
+                    height: 64,
+                  },
+                },
+              },
+            ),
           },
           {
-            tabBarOptions: {
-              keyboardHidesTabBar: true,
-              activeTintColor: '#FFF',
-              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
-              labelStyle: {
-                fontSize: 12,
-                marginTop: 5,
-              },
-              style: {
-                backgroundColor: '#2B1A2F',
-                paddingHorizontal: 50,
-                paddingTop: 13,
-                paddingBottom: 12,
-                borderTopWidth: 0,
-                height: 64,
-              },
-            },
+            defaultNavigationOptions: () => ({
+              headerTransparent: true,
+              headerTitle: <Header />,
+            }),
           },
         ),
       },
