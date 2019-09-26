b658c4b63009302a7b01453e3daf8791dd046abe
Lucas Weyne <weynelucas@gmail.com>
2019-09-26 07:58:17 -0300

    chore: login redirect

--- app/src/index.js
+++ app/src/index.js
@@ -7,17 +7,15 @@ import './config/ReactotronConfig';
 
 import { store, persistor } from './store';
 
-import Routes from './routes';
+import App from './App';
 
-const App = () => {
+export default () => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <StatusBar barStyle="light-content" backgroundColor="#000" ba />
-        <Routes />
+        <StatusBar barStyle="light-content" backgroundColor="#000" />
+        <App />
       </PersistGate>
     </Provider>
   );
 };
-
-export default App;
