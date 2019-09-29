9d65d4a9c568ff7f6475d9f80e7f9190ff0ac81e
Lucas Weyne <weynelucas@gmail.com>
2019-09-29 10:54:29 -0300

    feat: unsubscribe meetup

--- mobile/src/pages/Subscriptions/index.js
+++ mobile/src/pages/Subscriptions/index.js
@@ -1,12 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { format, parseISO, isAfter } from 'date-fns';
+import locale from 'date-fns/locale/pt-BR';
+
+import api from '~/services/api';
 
 import Background from '~/components/Background';
+import Meetup from '~/components/Meetup';
 
-// import { Container } from './styles';
+import { Container, List, UnsubscribeButton } from './styles';
 
 export default function Subscriptions() {
-  return <Background />;
+  const [subscriptions, setSubscriptions] = useState([]);
+  const [isUnsubscribing, setIsUnsubscribing] = useState(false);
+
+  async function loadSubscriptions() {
+    const response = await api.get('/subscriptions');
+
+    const data = response.data.map(({ id, meetup }) => ({
+      id,
+      meetup: {
+        ...meetup,
+        dateFormatted: format(
+          parseISO(meetup.date),
+          "dd 'de' MMMM', às' HH'h'",
+          { locale },
+        ),
+      },
+    }));
+
+    setSubscriptions(data);
+  }
+
+  function canUnsubscribe({ date }) {
+    return isAfter(parseISO(date), new Date());
+  }
+
+  async function handleUnsubscripe(subscriptionId) {
+    try {
+      setIsUnsubscribing(true);
+      await api.delete(`/subscriptions/${subscriptionId}/`);
+
+      setSubscriptions(subscriptions.filter(s => s.id !== subscriptionId));
+    } catch (err) {
+      Alert.alert('Falha ao cancelar inscrição', err.response.data.error);
+    } finally {
+      setIsUnsubscribing(false);
+    }
+  }
+
+  useEffect(() => {
+    loadSubscriptions();
+  }, []);
+
+  return (
+    <Background>
+      <Container>
+        <List
+          data={subscriptions}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <Meetup meetup={item.meetup}>
+              {canUnsubscribe(item.meetup) ? (
+                <UnsubscribeButton
+                  loading={isUnsubscribing}
+                  onPress={() => handleUnsubscripe(item.id)}>
+                  Cancelar inscrição
+                </UnsubscribeButton>
+              ) : (
+                <></>
+              )}
+            </Meetup>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 Subscriptions.navigationOptions = {
--- mobile/src/pages/Subscriptions/styles.js
+++ mobile/src/pages/Subscriptions/styles.js
@@ -2,24 +2,19 @@ import styled from 'styled-components/native';
 import { FlatList } from 'react-native-gesture-handler';
 
 import Button from '~/components/Button';
-import DatePicker from '~/components/DatePicker';
 
 export const Container = styled.View`
   flex: 1;
   padding: 82px 20px 0 20px;
 `;
 
-export const Header = styled(DatePicker)`
-  align-self: stretch;
-  margin: 10px 0;
-`;
-
 export const List = styled(FlatList).attrs({
   showScrollIndicator: false,
 })``;
 
-export const SubscribeButton = styled(Button).attrs({
+export const UnsubscribeButton = styled(Button).attrs({
   size: 'small',
 })`
   margin-top: 15px;
+  background-color: #d44059;
 `;
