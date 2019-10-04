a164fdc1c454a73cc9504641ec7c46aeed6d3658
Lucas Weyne <weynelucas@gmail.com>
2019-10-04 17:15:22 -0300

    refactor: remove pagination from organizing controller

--- api/src/app/controllers/OrganizingController.js
+++ api/src/app/controllers/OrganizingController.js
@@ -1,39 +1,21 @@
 import Meetup from '../models/Meetup';
 import File from '../models/File';
-import PaginationService from '../services/PaginationService';
 
 class OrganizingController {
   async index(req, res) {
-    const limit = 10;
-    const { page = 1 } = req.query;
-
-    const {
-      results,
-      count,
-      nextPage,
-      previousPage,
-    } = await PaginationService.run(
-      Meetup,
-      {
-        where: { userId: req.user.id },
-        attributes: ['id', 'title', 'description', 'date', 'location'],
-        include: [
-          {
-            model: File,
-            as: 'banner',
-            attributes: ['id', 'name', 'path', 'url'],
-          },
-        ],
-      },
-      { page, pageSize: limit }
-    );
-
-    return res.json({
-      count,
-      previousPage,
-      nextPage,
-      results,
+    const meetups = await Meetup.findAll({
+      where: { userId: req.user.id },
+      attributes: ['id', 'title', 'description', 'date', 'location'],
+      include: [
+        {
+          model: File,
+          as: 'banner',
+          attributes: ['id', 'name', 'path', 'url'],
+        },
+      ],
     });
+
+    return res.json(meetups);
   }
 }
 
