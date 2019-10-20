850a9eadd3e2b09c3a3b630ff05d715a638c35b2
Lucas Weyne <lucasweynebarrosferreira@MacBook-Pro-de-Lucas.local>
2019-10-20 17:13:52 -0300

    fix: RCTRootView cancelTouches warning disable

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { YellowBox, StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 
 import { store, persistor } from './store';
 
 import App from './App';
+
+YellowBox.ignoreWarnings(['RCTRootView cancelTouches']);
 
 export default () => {
   return (
