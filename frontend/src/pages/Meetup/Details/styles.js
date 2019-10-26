34ac92fcae47e36fe7fed03bde482312046ba2c5
Lucas Weyne <lucasweynebarrosferreira@MacBook-Pro-de-Lucas.local>
2019-10-26 11:52:43 -0300

    styles: add margin at meetup details

--- frontend/src/pages/Meetup/Details/styles.js
+++ frontend/src/pages/Meetup/Details/styles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  margin-bottom: 20px;
+`;
 
 export const MeetupHeader = styled.h1`
   display: flex;
